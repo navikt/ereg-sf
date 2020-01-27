f6968b79ed10e8c32058e3381c93f9a7217855f9
torsteinnesby <ttnesby@gmail.com>
2020-01-27 11:44:09 +0100

    Fix kafka exception caused by prometheus

--- src/main/kotlin/no/nav/ereg/Work.kt
+++ src/main/kotlin/no/nav/ereg/Work.kt
@@ -1,12 +1,19 @@
 package no.nav.ereg
 
 import mu.KotlinLogging
+import no.nav.ereg.proto.EregOrganisationEventKey
+import no.nav.ereg.proto.EregOrganisationEventValue
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 
 private val log = KotlinLogging.logger {}
 
 internal fun work(ev: EnvVar) {
+
+    data class OrgObject(
+        val key: EregOrganisationEventKey,
+        val value: EregOrganisationEventValue
+    )
 
     log.info { "bootstrap work session starting" }
 
@@ -17,20 +24,26 @@ internal fun work(ev: EnvVar) {
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
             ConsumerConfig.GROUP_ID_CONFIG to ev.kafkaClientID,
             ConsumerConfig.CLIENT_ID_CONFIG to ev.kafkaClientID,
-            // ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
             ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to "false"
         ).let { cMap ->
             if (ev.kafkaSecurityEnabled())
                 cMap.addKafkaSecurity(ev.kafkaUser, ev.kafkaPassword, ev.kafkaSecProt, ev.kafkaSaslMec)
             else cMap
         },
-        listOf(ev.kafkaTopic),
-        fromBeginning = true // TODO use this in the beginning, test purpose, otherwise false
+        listOf(ev.kafkaTopic)
     ) { cRecords ->
         if (!cRecords.isEmpty) {
-            Metrics.sentOrgs.inc(cRecords.count().toDouble())
-            // TODO post records to SF, with error handling
-            // TODO feedback to KafkaDSL should be IsOk in order to commit
+            cRecords
+                .map { OrgObject(it.key().protobufSafeParseKey(),it.value().protobufSafeParseValue()) }
+                .filter { it.key.orgNumber.isNotEmpty() && it.value.orgAsJson.isNotEmpty()}
+                .let { orgObjects ->
+                    // TODO post records to SF, with error handling
+                    // TODO feedback to KafkaDSL should be IsOk in order to commit
+                    orgObjects.forEach {
+                        Metrics.sentOrgs.labels(it.key.orgType.toString()).inc()
+                    }
+                }
             ConsumerStates.IsOkNoCommit
         } else {
             log.info { "Kafka events completed for now - leaving kafka consumer loop" }
